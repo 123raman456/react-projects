6f1af50e67be98696d49fd173b20fa4148aa44f9
Raman Verma <123955950+123raman456@users.noreply.github.com>
2023-02-26 16:36:43 +0530

    Update index.js

--- React-QuizApp/src/index.js
+++ React-QuizApp/src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import {questions} from "./dataQuestions";
+import { questions } from "./dataQuestions";
 import "./App.js";
 import QuizApp from "./App.js";
 
@@ -25,44 +25,55 @@ function Text() {
       setShowRes(true);
     }
   };
-  const handleRestart=()=>{
+  const handleRestart = () => {
     setCurrQuestion(0);
     setScore(0);
     setCorrAns(0);
     setShowRes(false);
-  }
+  };
+
   return (
     <>
       <div className="app">
-        {showRes?(<QuizApp score={score} corrAns={corrAns} handleRestart={handleRestart}/>):(<>
-        <div className="question-area">
-          <h5>Score: {score}</h5>
-        
-        <div className="question-count">
-          <h2>
-            QuestionArea {currQuestion + 1}/{questions.length}
-          </h2>
-          </div>
-        
-        <div className="question-text">
-          {questions[currQuestion].QuestionArea}
-        </div>
-        </div>
-        <div className="answer-area">
-          {questions[currQuestion].answerOption.map((ans, i) => {
-            return (
-              <button key={i} onClick={() => handleansOption(ans.isCorrect)}>
-                {ans.answerText}
-              </button>
-            );
-          })}
-          <div className="button">
-            <button onClick={handleRestart}>Quit</button>
-            <button onClick={handleNextOpt}>Next</button>
-          </div>
-        </div>
-        </>)}
-        
+        {showRes ? (
+          <QuizApp
+            score={score}
+            corrAns={corrAns}
+            handleRestart={handleRestart}
+          />
+        ) : (
+          <>
+            <div className="question-area">
+              <h5>Score: {score}</h5>
+
+              <div className="question-count">
+                <h2>
+                  Question {currQuestion + 1}/{questions.length}
+                </h2>
+              </div>
+
+              <div className="question-text">
+                {questions[currQuestion].QuestionArea}
+              </div>
+              <div className="answer-area">
+                {questions[currQuestion].answerOption.map((ans, i) => {
+                  return (
+                    <button
+                      key={i}
+                      onClick={() => handleansOption(ans.isCorrect)}
+                    >
+                      {ans.answerText}
+                    </button>
+                  );
+                })}
+                <div className="button">
+                  <button onClick={handleRestart}>Quit</button>
+                  <button onClick={handleNextOpt}>Next</button>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
